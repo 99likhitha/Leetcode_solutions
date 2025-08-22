56cac0993e74cc5e974276dbc82ddd45057c8004
Likhithak <106301685+99likhitha@users.noreply.github.com>
2025-08-22 15:55:33 -0400

    Time: 3 ms (41.96%), Space: 19.5 MB (74.72%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int findparent(vector<int>&parent,int x)
+    {
+        if(parent[x]==x) return x;
+        return parent[x]=findparent(parent,parent[x]);
+    }
+
+    void Union(vector<int>&parent,int x,int y)
+    {
+       int parent_x=findparent(parent,x);
+       int parent_y=findparent(parent,y);
+       if(parent_x !=parent_y) parent[parent_x]=y; 
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<int>parent(n);
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+        }
+       
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<isConnected[0].size();j++)
+            {
+                if(isConnected[i][j]==1 && i!=j)
+                {
+                    Union(parent,i,j);
+                }
+                
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=findparent(parent,i);
+        }
+
+
+        set<int>s;
+        for(int i=0;i<parent.size();i++)
+        {
+            s.insert(parent[i]);
+        }
+
+
+        return s.size();
+
+
+    }
+};
