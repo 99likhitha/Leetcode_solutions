1d131d579d7bcbaa32cdf6e32ccd9caf8815333b
Likhithak <106301685+99likhitha@users.noreply.github.com>
2025-11-12 13:18:27 -0500

    Time: 0 ms (100%), Space: 31.8 MB (34.03%) - LeetHub

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
+    int gcd(int a ,int b)
+    {
+        return b==0 ? a: gcd(b,a%b);
+    }
     int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        int num1 = 0;
-        int g = 0;
-
-        // Count number of 1's and compute overall GCD
-        for (int x : nums) {
-            if (x == 1)
-                num1++;
-            g = gcd(g, x);
+        int n=nums.size();
+        int curr;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==1) count++;
         }
+        if(count>0) return n-count;
 
-        // Case 1: already contains 1s
-        if (num1 > 0)
-            return n - num1;
-
-        // Case 2: impossible if overall GCD > 1
-        if (g > 1)
-            return -1;
-
-        // Case 3: find shortest subarray with GCD = 1
-        int min_len = n;
-        for (int i = 0; i < n; i++) {
-            int curr_g = 0;
-            for (int j = i; j < n; j++) {
-                curr_g = gcd(curr_g, nums[j]);
-                if (curr_g == 1) {
-                    min_len = min(min_len, j - i + 1);
+        int minlen=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            curr=nums[i];
+            for(int j=i+1;j<n;j++)
+            {
+                curr=gcd(curr,nums[j]);
+                if(curr==1)
+                {
+                    minlen=min(minlen,j-i+1);
                     break;
                 }
             }
         }
 
-        // total operations = (min_len - 1) to create 1 + (n - 1) to spread it
-        return min_len + n - 2;
+        if(minlen == INT_MAX) return -1;
+
+        return minlen-1 + (n-1);
     }
 };
