7ae29ab48383107952f5f738bfce6d0dfeccfb64
Likhithak <106301685+99likhitha@users.noreply.github.com>
2025-11-14 12:28:44 -0500

    Time: 7 ms (43.07%), Space: 15.9 MB (54.38%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int ones = 0, res = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '1')
-                ones++;
-            else if ((i > 0) && s[i - 1] == '1')
-                res += ones;
+        int n=s.size();
+        int result=0;
+        int cars=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='1')
+            {
+                cars++;
+            }
+            else if( i>0 && s[i-1] =='1')
+            {
+                result+=cars;
+            }
         }
 
-        return res;
+        return result;
     }
 };
