a27a4585b02e5d77ae39cc319118d3853948af2b
Likhithak <106301685+99likhitha@users.noreply.github.com>
2025-11-24 16:30:44 -0500

    Time: 0 ms (100%), Space: 18.6 MB (7.85%) - LeetHub

--- 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
+const int MOD=1e9;
     vector<bool> prefixesDivBy5(vector<int>& nums) {
-        vector<bool> ans;
-        int val = 0;
-        for (int bit : nums) {
-            val = (val * 2 + bit) % 5;
-            ans.push_back(val == 0);
+        int n=nums.size();
+        vector<bool>ans(n,0);
+        long long val=0;
+        int p=1;
+        for(int i=0;i<n;i++)
+        {
+            val%=MOD;
+            if(nums[i] == 1)
+            {
+                val=val*2+1;
+            }
+            else
+            {
+                val*=2;
+            }
+            if(val%5==0) 
+            {
+                ans[i]=true;
+            }
+            else ans[i]=false;
         }
         return ans;
     }
