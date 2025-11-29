a0385cd5393a400ac1c32cfe3cf347c2481bfd11
Likhithak <106301685+99likhitha@users.noreply.github.com>
2025-11-29 06:55:49 -0500

    Time: 0 ms (100%), Space: 45.3 MB (69.81%) - LeetHub

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int sum = 0;
-        for(auto x : nums) sum += x;
-        return sum % k;
+        int n=nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+
+        return sum%k;
     }
 };
