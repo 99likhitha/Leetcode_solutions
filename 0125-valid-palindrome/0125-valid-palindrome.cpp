136806b1677bc5da9593d9507fb24468bc34e4a5
Likhithak <106301685+99likhitha@users.noreply.github.com>
2025-12-04 16:42:30 -0500

    Time: 2 ms (40.32%), Space: 10.6 MB (8.91%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n=s.size();
+        string pal="";
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]!=' ' && isalnum(s[i]))
+            {
+                pal+= tolower(s[i]);
+            }
+        }
+
+        // cout<<pal<<endl;
+        string rev=pal;
+        reverse(pal.begin(),pal.end());
+        return (rev==pal);
+    }
+};
