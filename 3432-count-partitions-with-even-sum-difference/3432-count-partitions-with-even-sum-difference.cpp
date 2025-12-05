633d64fd752c497eb267aba3d4a91372c5460d22
Likhithak <106301685+99likhitha@users.noreply.github.com>
2025-12-05 17:49:33 -0500

    Time: 0 ms (100%), Space: 22.6 MB (25%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     int countPartitions(vector<int>& A) {
-        int total = accumulate(A.begin(), A.end(), 0);
-        return total & 1 ? 0 : A.size() - 1;
+        int leftsum=0;
+        int n=A.size();
+       for(int num : A)
+       {
+        leftsum+=num;
+       }
+
+        int rightsum=0;
+        int count=0;
+       for(int i=n-1;i>0;i--)
+       {
+        rightsum+=A[i];
+        leftsum-=A[i];
+
+        if(abs(rightsum-leftsum)%2==0) count++;
+       }
+
+       return count;
     }
 };
