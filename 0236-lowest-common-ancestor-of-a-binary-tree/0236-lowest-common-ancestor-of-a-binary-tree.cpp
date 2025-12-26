9a3ff4a26f235263c7869d2b9d38691d9fc528ca
Likhithak <106301685+99likhitha@users.noreply.github.com>
2025-12-26 16:39:42 -0500

    Time: 28 ms (5.69%), Space: 19 MB (5.34%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,15 +9,40 @@
  */
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL) return NULL;
-        if(p->val == root->val || q->val==root->val) return root;
-        TreeNode*leftLCA=lowestCommonAncestor(root->left,p,q);
-        TreeNode*rightLCA=lowestCommonAncestor(root->right,p,q);
+    bool fillup(TreeNode* root,TreeNode* node,vector<TreeNode*>&parent){
+        if(root==NULL)return false;
+        // cout<<root->val<<endl;
+        parent.push_back(root);
+        if(root==node)return true;
+        if(fillup(root->left,node,parent)||fillup(root->right,node,parent)){
+            return true;
+        }
+        // cout<<node->val<<", "<<parent.back()->val<<endl;
+        parent.pop_back();
+        return false;
 
-        if(leftLCA && rightLCA) return root;
-        if(leftLCA && !rightLCA) return leftLCA;
-        if(!leftLCA && rightLCA) return rightLCA;
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* a, TreeNode* b) {
+        vector<TreeNode*>Pa,Pb;
+
+        fillup(root,a,Pa);
+        fillup(root,b,Pb);
+        // {
+            int n=Pa.size();
+            int m=Pb.size();
+            // cout<<n<<","<<m<<endl;
+            int i=0;
+            int j=0;
+            while(i<n&&j<m){
+                cout<<Pa[i]<<","<<Pb[j]<<endl;
+                if(Pa[i]!=Pb[j])return Pa[i-1];
+                i++;
+                j++;
+            }
+            if(i<n)return Pa[i-1];
+            else if(j<m)return Pb[j-1];
+        // }
 
         return NULL;
 
