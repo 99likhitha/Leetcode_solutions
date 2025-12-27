80a22cf41977f90053927e77c100a7a80dfa48a9
Likhithak <106301685+99likhitha@users.noreply.github.com>
2025-12-26 22:11:10 -0500

    Time: 0 ms (100%), Space: 13.6 MB (75.99%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -5,17 +5,14 @@ public:
         int low=0,high=n-1;
         while(low<=high)
         {
-            int mid=low + (high-low)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[mid]<target)
+            else if(nums[mid]>target)
             {
-                low=mid+1;
-            }
-            else{
                 high=mid-1;
             }
+            else low=mid+1;
         }
-
         return low;
     }
 };
