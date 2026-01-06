e5e9bd8eb9dff3c7351b126c2b8316d64a03ee28
Likhithak <106301685+99likhitha@users.noreply.github.com>
2026-01-06 10:54:13 -0500

    Time: 0 ms (100%), Space: 39.2 MB (61.72%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int count=0;
+        int minele=INT_MAX;
         long long sum=0;
-        int minAbs=INT_MAX, cntNeg=0;
-        for(auto& row: matrix){
-            for(int x: row){
-                minAbs=min(minAbs, abs(x));
-                if (x<0){
-                    sum-=x;
-                    cntNeg++;
+        bool iszero=false;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]<0)
+                {
+                    count++;
                 }
-                else{
-                    sum+=x;
-                }
+                minele=min(minele,abs(matrix[i][j]));
+                sum+=abs(matrix[i][j]);
+                if(matrix[i][j]==0) iszero=true;
             }
         }
-        return (cntNeg&1)?sum-2*minAbs:sum;
+
+        if(count%2==0) return sum;
+        else 
+        {
+            if(iszero) return sum;
+        }
+        return sum-(2*minele);
+
     }
 };
