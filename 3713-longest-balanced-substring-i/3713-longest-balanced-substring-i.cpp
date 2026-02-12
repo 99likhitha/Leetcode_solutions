268aedb147eba37fb7215cd43cf81922e07ce796
Likhithak <106301685+99likhitha@users.noreply.github.com>
2026-02-12 14:39:13 -0500

    Time: 268 ms (18.4%), Space: 41.7 MB (14.62%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -4,22 +4,18 @@ public:
         int n=s.length();
         int maxlen=0;
         for(int i=0;i<n;i++)
-        {
-           
+        { 
+            int maxfreq=0;
             unordered_map<char,int>mp;
             for(int j=i;j<n;j++)
             {
-                if(mp.find(s[j]) != mp.end())
+                mp[s[j]]++;  
+                maxfreq=max(maxfreq,mp[s[j]]);
+                
+                if(mp.size() * maxfreq == j-i+1)
                 {
-                    mp[s[j]]++;  
+                 maxlen=max(maxlen,j-i+1);
                 }
-                int count=mp[s[j]];
-                int flag=true;
-                for(auto itr: mp)
-                {
-                    if(itr.second !=count) flag=false;
-                }
-                if(flag) maxlen=max(maxlen,j-i+1);
             }
         }
 
