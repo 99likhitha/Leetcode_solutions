f9d3fe0ea30dced8d4e28131507693a21d4f7d06
Likhithak <106301685+99likhitha@users.noreply.github.com>
2026-04-19 17:34:57 -0400

    Time: 0 ms (100%), Space: 9.8 MB (55.67%) - LeetHub

--- 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.cpp
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    bool canTransform(string start, string end) {
-    int n = start.size();
-    
-    int i = 0, j = 0;
-    
-    while (i < n || j < n) {
-        while (i < n && start[i] == 'X') i++;
-        while (j < n && end[j] == 'X') j++;
-        
-        if (i == n && j == n) return true;
-        if (i == n || j == n) return false;
-        
-        if (start[i] != end[j]) return false;
-        
-        if (start[i] == 'L' && i < j) return false;
-        if (start[i] == 'R' && i > j) return false;
-        
-        i++; j++;
-    }
-    return true;
-    
+    bool canTransform(string start, string result) {
+        int n=start.size();
+        int m=result.size();
+        int i=0,j=0;
+        while(i < n || j < m)
+        {
+            while(i<n && start[i]=='X') i++;
+            while(j<m && result[j] =='X') j++;
+
+            if(i ==n && j== m ) return true;
+            if(i==n || j== m) return false;
+
+            if(start[i] != result[j]) return false;
+
+            if(start[i] == 'L' && i<j) return false;
+            if(start[i] =='R' && i>j) return false;
+
+            i++;
+            j++;
+        }
+
+        return true;
     }
 };
