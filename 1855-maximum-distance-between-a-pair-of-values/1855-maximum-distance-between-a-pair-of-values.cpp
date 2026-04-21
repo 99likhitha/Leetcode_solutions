067d96348d0d50dc860a54177d2a67d3f143c5c3
Likhithak <106301685+99likhitha@users.noreply.github.com>
2026-04-20 20:59:12 -0400

    Time: 0 ms (100%), Space: 102 MB (98.37%) - LeetHub

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
+        int maxdist=0;
+        int i=0,j=0;
+        while(i<n && j<m)
+        {
+            if(nums1[i] <=nums2[j])
+            {
+                maxdist=max(maxdist, j-i);
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        return maxdist;
+    }
+};
